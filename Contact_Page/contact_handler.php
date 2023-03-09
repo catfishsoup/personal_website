69c6078b4dea8e288492226a804f4d4cf972dda9
Jen Nguyen <jennifernguyen0000@gmail.com>
2023-03-09 13:19:10 -0500

    Added folder directory

---
+++ Contact_Page/contact_handler.php
@@ -0,0 +1,15 @@
+<?php 
+
+if(isset($_POST["submit"])) {
+    $email = $_POST["email"];
+    $name = $_POST["fname"]; 
+    $mess = $_POST["user_mess"]; 
+
+    $recipient = "jennifernguyen0000@gmail.com";
+    $headers = "Message from ".$email." sent via Personal Portfolio website."; 
+    $txt = "You have received an email from".$name.".\n\n".$mess;
+
+    mail($recipient, "Contact from Personal Portfolio Website", $txt, $headers); 
+    header("Location: contact.php?mailsend");
+}
+?>
